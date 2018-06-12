94a32b30737fbf94d12053ccc96021cb159f2b6b
brusoman <brusoman@gmail.com>
2018-06-12 17:16:00 +0300

    Create component + view component

---
+++ angular-components/src/app/components/select-button/select-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'select-button-component',
+  templateUrl: './select-button.component.html',
+  styleUrls: ['./select-button.component.scss']
+})
+export class SelectButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angular-components/src/app/showcase/select-button-view/select-button-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'select-button-view',
+  templateUrl: './select-button-view.component.html',
+  styleUrls: ['./select-button-view.component.scss']
+})
+export class SelectButtonViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
