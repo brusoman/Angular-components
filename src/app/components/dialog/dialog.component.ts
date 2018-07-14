b9d2d7ca6fd86c692fd0751b363dda3febea5f7a
brusoman <brusoman@gmail.com>
2018-07-14 14:28:39 +0300

    Add view + css

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {DialogModel} from "./dialog.model";
 
 @Component({
   selector: 'dialog-component',
@@ -6,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
-
+  @Input() model: DialogModel;
   constructor() {
   }
 
--- src/app/showcase/dialog-showcase/dialog-showcase.component.ts
+++ src/app/showcase/dialog-showcase/dialog-showcase.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {TEST_DIALOG} from "../../components/dialog/dialog.model";
 
 @Component({
   selector: 'dialog-showcase',
@@ -6,11 +7,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./dialog-showcase.component.scss']
 })
 export class DialogShowcaseComponent implements OnInit {
-
+  model
   constructor() {
   }
 
   ngOnInit() {
+    this.model = JSON.parse(JSON.stringify(TEST_DIALOG));
+
   }
 
 }
