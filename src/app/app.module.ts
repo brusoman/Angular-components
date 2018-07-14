4a65136abf7f0124be6c66dbd681e1d09c13eb80
brusoman <brusoman@gmail.com>
2018-07-14 15:54:19 +0300

    CSS buttons,title, text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {SelectButtonComponent} from './components/select-button/select-button.component';
-import {SelectButtonViewComponent} from './showcase/select-button-view/select-button-view.component';
+import {SelectButtonViewComponent} from './showcase/select-button-showcase/select-button-view.component';
 import {DialogComponent} from './components/dialog/dialog.component';
 import {DialogShowcaseComponent} from './showcase/dialog-showcase/dialog-showcase.component';
-import {SelectButtonViewComponent} from './showcase/select-button-showcase/select-button-view.component';
 import {GrowlComponent} from './components/growl/growl-item/growl.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {GrowlMasterComponent} from './components/growl/growl-master/growl-master.component';
@@ -18,7 +17,7 @@ import {GrowlShowcaseComponent} from './showcase/growl-showcase/growl-showcase.c
     SelectButtonComponent,
     SelectButtonViewComponent,
     DialogComponent,
-    DialogShowcaseComponent
+    DialogShowcaseComponent,
     SelectButtonViewComponent,
     GrowlComponent,
     GrowlMasterComponent,
@@ -31,4 +30,5 @@ import {GrowlShowcaseComponent} from './showcase/growl-showcase/growl-showcase.c
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
